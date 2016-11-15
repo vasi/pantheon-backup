853f02f588ec35bb3449d30d4a0273bbb8cecb78
WuCris <chris_home@live.ca>
2016-11-15 15:00:45 -0500

    initial backup script

---
+++ pantheon-backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+BACKUP_FOLDER=/var/shared/assets
+SKIP_TABLES=cache_bootstrap,cache_config,cache_container,cache_data,cache_default,cache_discovery,cache_dynamic_page_cache,cache_entity,cache_menu,cache_render,cachetags,watchdog,sessions,search_index,search_dataset,search_total
+
+
+while read -r ALIAS DIRECTROY _; do
+    drush "$ALIAS" sql-dump --skip-tables-list "$SKIP_TABLES" > $BACKUP_FOLDER/$DIRECTROY/"$(echo $ALIAS | grep -Pe '[^.]*$' -o)"-db-$(date +"%Y-%m-%d_%H-%M").sql
+done < <(grep -v '^$\|^\s*\#' /etc/pantheon-backup-sites)
