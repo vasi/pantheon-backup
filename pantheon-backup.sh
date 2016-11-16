883fa8dc8914f9fd07c5177cc7a62050df29808a
WuCris <chris_home@live.ca>
2016-11-16 15:12:20 -0500

    add create directroy if non existant

--- pantheon-backup.sh
+++ pantheon-backup.sh
@@ -7,5 +7,6 @@ SKIP_TABLES=cache_bootstrap,cache_config,cache_container,cache_data,cache_defaul
 
 
 while read -r ALIAS DIRECTROY _; do
+    mkdir -p $BACKUP_FOLDER/$DIRECTROY/
     drush "$ALIAS" sql-dump --skip-tables-list "$SKIP_TABLES" > $BACKUP_FOLDER/$DIRECTROY/"$(echo $ALIAS | grep -Pe '[^.]*$' -o)"-db-$(date +"%Y-%m-%d_%H-%M").sql
 done < <(grep -v '^$\|^\s*\#' /etc/pantheon-backup-sites)
