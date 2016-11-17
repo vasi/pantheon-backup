da715281baa57a16ce8feed462c519856fd6965c
Dave Vasilevsky <dave@vasilevsky.ca>
2016-11-17 16:35:33 -0500

    Allow trailing comments

--- pantheon-backup.sh
+++ pantheon-backup.sh
@@ -6,7 +6,7 @@ CONFIG_FILE=${CONFIG_FILE:-/etc/pantheon-backup-sites}
 BACKUP_FOLDER=${BACKUP_FOLDER:-/var/shared/assets}
 SKIP_TABLES=cache_bootstrap,cache_config,cache_container,cache_data,cache_default,cache_discovery,cache_dynamic_page_cache,cache_entity,cache_menu,cache_render,cachetags,watchdog,sessions,search_index,search_dataset,search_total
 
-grep -E '^[^#]+' "$CONFIG_FILE" | while read CONFIGS; do
+grep -o -E '^[^#]+' "$CONFIG_FILE" | while read CONFIGS; do
     ALIAS="$(echo "$CONFIGS" | awk -F: '{print $1}')"
     DIRECTORY="$(echo "$CONFIGS" | awk -F: '{print $2}')"
     BRANCH_NAME="$(echo "$ALIAS" | awk -F. '{print $NF}')"
