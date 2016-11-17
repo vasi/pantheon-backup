7c20a2d770ce4c4259e21339dd7bc7d03ee875af
WuCris <chris_home@live.ca>
2016-11-17 14:30:06 -0500

    use for loop and improve code

--- pantheon-backup.sh
+++ pantheon-backup.sh
@@ -6,12 +6,15 @@ BACKUP_FOLDER=/var/shared/assets
 SKIP_TABLES=cache_bootstrap,cache_config,cache_container,cache_data,cache_default,cache_discovery,cache_dynamic_page_cache,cache_entity,cache_menu,cache_render,cachetags,watchdog,sessions,search_index,search_dataset,search_total
 
 
-while read -r ALIAS DIRECTROY _; do
+for CONFIGS in $(grep -v '^$\|^\s*\#' /etc/pantheon-backup-sites); do
 
-    BRANCH_NAME="$(echo $ALIAS | grep -Pe '[^.]*$' -o)"
+    ALIAS="awk -F ":" '{print \$1}' < <(echo $CONFIGS)"
+    DIRECTROY="awk -F ":" '{print \$2}' < <(echo $CONFIGS)"
 
-    mkdir -p $BACKUP_FOLDER/$DIRECTROY/archive/$BRANCH_NAME
-    drush "$ALIAS" sql-dump --skip-tables-list "$SKIP_TABLES" > $BACKUP_FOLDER/$DIRECTROY/archive/$BRANCH_NAME/$BRANCH_NAME-db-$(date +"%Y-%m-%d_%H-%M").sql
-    ln -sf $BACKUP_FOLDER/$DIRECTROY/archive/$BRANCH_NAME/$(ls $BACKUP_FOLDER/$DIRECTROY/archive/$BRANCH_NAME -t | head -n1) $BACKUP_FOLDER/$DIRECTROY/$BRANCH_NAME.latest.sql
+    BRANCH_NAME="$(eval $ALIAS | grep -Pe '[^.]*$' -o)"
 
-done < <(grep -v '^$\|^\s*\#' /etc/pantheon-backup-sites)
+    mkdir -p $BACKUP_FOLDER/$(eval $DIRECTROY)/archive/$BRANCH_NAME
+    drush "$(eval $ALIAS)" sql-dump --skip-tables-list "$SKIP_TABLES" > $BACKUP_FOLDER/$(eval $DIRECTROY)/archive/$BRANCH_NAME/$BRANCH_NAME-db-$(date +"%Y-%m-%d_%H-%M").sql
+    ln -sf $BACKUP_FOLDER/$(eval $DIRECTROY)/archive/$BRANCH_NAME/$(ls $BACKUP_FOLDER/$(eval $DIRECTROY)/archive/$BRANCH_NAME -t | head -n1) $BACKUP_FOLDER/$(eval $DIRECTROY)/$BRANCH_NAME.latest.sql
+
+done
