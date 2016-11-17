7cc8545124fe985f0f0913f162b423ca95315e06
Dave Vasilevsky <dave@vasilevsky.ca>
2016-11-17 16:28:29 -0500

    Get rid of evals

--- pantheon-backup.sh
+++ pantheon-backup.sh
@@ -7,13 +7,15 @@ BACKUP_FOLDER=${BACKUP_FOLDER:-/var/shared/assets}
 SKIP_TABLES=cache_bootstrap,cache_config,cache_container,cache_data,cache_default,cache_discovery,cache_dynamic_page_cache,cache_entity,cache_menu,cache_render,cachetags,watchdog,sessions,search_index,search_dataset,search_total
 
 grep -E '^[^#]+' "$CONFIG_FILE" | while read CONFIGS; do
-    ALIAS="awk -F ":" '{print \$1}' < <(echo $CONFIGS)"
-    DIRECTORY="awk -F ":" '{print \$2}' < <(echo $CONFIGS)"
+    ALIAS="$(echo "$CONFIGS" | awk -F: '{print $1}')"
+    DIRECTORY="$(echo "$CONFIGS" | awk -F: '{print $2}')"
+    BRANCH_NAME="$(echo "$ALIAS" | awk -F. '{print $NF}')"
 
-    BRANCH_NAME="$(eval $ALIAS | grep -Pe '[^.]*$' -o)"
+    DIR="$BACKUP_FOLDER/$DIRECTORY/archive/$BRANCH_NAME"
+    mkdir -p "$DIR"
 
-    mkdir -p $BACKUP_FOLDER/$(eval $DIRECTORY)/archive/$BRANCH_NAME
-    drush "$(eval $ALIAS)" sql-dump --skip-tables-list "$SKIP_TABLES" > $BACKUP_FOLDER/$(eval $DIRECTORY)/archive/$BRANCH_NAME/$BRANCH_NAME-db-$(date +"%Y-%m-%d_%H-%M").sql
-    ln -sf $BACKUP_FOLDER/$(eval $DIRECTORY)/archive/$BRANCH_NAME/$(ls $BACKUP_FOLDER/$(eval $DIRECTORY)/archive/$BRANCH_NAME -t | head -n1) $BACKUP_FOLDER/$(eval $DIRECTORY)/$BRANCH_NAME.latest.sql
-
+    DATE="$(date +%F-%R)"
+    FILE="$DIR/$BRANCH_NAME-$DATE.sql"
+    drush "$ALIAS" sql-dump --skip-tables-list "$SKIP_TABLES" > "$FILE"
+    ln -sfn "$FILE" "$DIR/$BRANCH_NAME.sql"
 done
